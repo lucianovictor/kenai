b191afb54492a0e6cb2d41837216242a43077247
Luciano Victor <lucianocarvallho7@gmail.com>
2022-05-19 16:11:53 -0300

    feat: adding fonts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -16,7 +16,11 @@ export default class MyDocument extends Document {
           <meta name="author" content="Luciano Victor" />
 
           <link
-            href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Mukta:wght@700&display=swap"
+            rel="stylesheet"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&display=swap"
             rel="stylesheet"
           />
         </Head>
